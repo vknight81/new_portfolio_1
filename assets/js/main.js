249abcfc4037e2c780ee51dec4a0d5bab0468af4
Vince Knight <vknight81@gmail.com>
2019-06-17 08:18:38 -0400

    formspree add

--- assets/js/main.js
+++ assets/js/main.js
@@ -352,27 +352,27 @@
 
 							});
 
-							var myform = $("form#myform");
+							// var myform = $("form#myform");
 
-							myform.submit(function(event){
-								event.preventDefault();
+// 							myform.submit(function(event){
+// 								event.preventDefault();
 
-							// Change to your service ID, or keep using the default service
-							var service_id = "default_service";
-							var template_id = "email_template";
+// 							// Change to your service ID, or keep using the default service
+// 							var service_id = "default_service";
+// 							var template_id = "email_template";
 
-							myform.find("button").text("Sending...");
-							emailjs.sendForm(service_id,template_id,"myform")
-							.then(function(){ 
-								alert("Sent!");
-								myform.find("button").text("Send Message");
-							}, function(err) {
-								alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-								myform.find("button").text("Send Message");
-							});
-							return false;
+// 							myform.find("button").text("Sending...");
+// 							emailjs.sendForm(service_id,template_id,"myform")
+// 							.then(function(){ 
+// 								alert("Sent!");
+// 								myform.find("button").text("Send Message");
+// 							}, function(err) {
+// 								alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+// 								myform.find("button").text("Send Message");
+// 							});
+// 							return false;
 
-});
+// });
 
 	});
 	
